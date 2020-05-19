c0fb27081f8741460d660d65adf87aecd57357cd
igordesouzamorais <igordesouzamorais@gmail.com>
2020-05-19 14:22:30 -0400

    corrigindo versoes no package e corrigindo warnings do mongoose no app.js

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const app = express();
 const cors = require('cors');
 const config = require('./app/config/config');
 
+//corrigir o warning do mongoose
+mongoose.set('useCreateIndex', true);
+
 // conexao com o banco de dados
-mongoose.connect(config.connectionString, {useNewUrlParser: true}, (err) => {
+mongoose.connect(config.connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
     if (err) console.log(err);
     
     console.log('banco de dados conectado');
@@ -21,6 +24,7 @@ const analistaRoute = require('./app/routes/usuario');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 // caminho das rotas
 
